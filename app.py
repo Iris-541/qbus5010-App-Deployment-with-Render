cc9a3e7f0d1fd3a85bb5128e7b001fefc5687ee1
Iris-541 <shsyw555@163.com>
2024-10-23 17:32:17 +1100

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def make_scatter_plot():
     return fig
 
 app.layout = html.Div(children=[
-    html.H1(children = "Restaurant Tips Exploratory Data Analysis", style={
+    html.H1(children = "Restaurant Tips Exploratory Data Analysis 11", style={
         "textAlign": "center",
         "font-size": "70px",
         "font-weight": "600",
